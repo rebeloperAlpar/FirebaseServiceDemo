a9c29cd38cde0ad7898275f2d65063a8c721c726
Alex Nagy <rebeloperalpar@gmail.com>
2023-03-07 17:08:06 +0200

    Initial Commit

---
+++ FirebaseServiceDemo/FirebaseServiceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseServiceDemoApp.swift
+//  FirebaseServiceDemo
+//
+//  Created by Alex Nagy on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseServiceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
